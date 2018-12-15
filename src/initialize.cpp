4553fc05e26516a2c08739c8fc3dcadaef14f93b
Ethan Wells <iuyte@disroot.org>
2018-12-15 15:00:15 -0500

    formatted to style

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -19,14 +19,14 @@ void initialize() {
   flipper.setBrakeMode(AbstractMotor::brakeMode::hold);
 
   // set the flywheel pid
-  //launcher.setVelPID(.4, .1, 0, .75);
+  // launcher.setVelPID(.4, .1, 0, .75);
   launcher.setVelPID(0, 2, 0, 7);
 
   // a task that prints a lot of useful data to the LCD emulator
   pros::Task(
           [](void *none) {
             // create a Label on the V5 screen
-            lv_obj_t * label1 =  lv_label_create(lv_scr_act(), NULL);
+            lv_obj_t *label1 = lv_label_create(lv_scr_act(), NULL);
 
             // change the Label's text
             lv_label_set_text(label1, "Hello world!");
@@ -38,10 +38,10 @@ void initialize() {
             auto text = std::string("speed: ");
 
             while (true) {
-              lv_label_set_text(label1, (text + std::to_string(launcher.getActualVelocity())).c_str());
+              lv_label_set_text(label1,
+                                (text + std::to_string(launcher.getActualVelocity())).c_str());
               controller.setText(
-                      2,
-                      0,
+                      2, 0,
                       std::string("v: ")
                               .append(std::to_string((int)(launcher.getActualVelocity() + .5)))
                               .append("   "));
@@ -59,8 +59,7 @@ void initialize() {
 void disabled() {
   // drive controller init: generate the motion path now (takes time) such that less precious time
   // during autonomous is lost
-  dc.generatePath({Point{0_ft, 0_ft, 0_deg},
-                   Point{0_ft, sqrt(2) * foot, 0_deg},
+  dc.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{0_ft, sqrt(2) * foot, 0_deg},
                    Point{0_ft, sqrt(2) * foot, -180_deg}},
                   "A");
 
